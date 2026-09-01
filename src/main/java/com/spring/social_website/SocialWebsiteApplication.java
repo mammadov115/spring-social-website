de3b5a267f1b9f02a35be5ac2714968a6b44aa31
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-01 15:07:20 +0400

    Initialize Spring Boot project with Maven wrapper and basic configuration files

---
+++ src/main/java/com/spring/social_website/SocialWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.spring.social_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialWebsiteApplication.class, args);
+	}
+
+}
