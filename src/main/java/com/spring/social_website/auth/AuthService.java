23f91fbcc3aae9a0272365201c5003e83af352b8
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-02 00:00:14 +0400

    Refactor login method to include refresh token handling and return access token

--- src/main/java/com/spring/social_website/auth/AuthService.java
+++ src/main/java/com/spring/social_website/auth/AuthService.java
@@ -5,10 +5,12 @@ import com.spring.social_website.auth.dto.LoginResponseDto;
 import com.spring.social_website.auth.dto.RegisterResponseDto;
 import com.spring.social_website.auth.dto.RegisterRequestDto;
 import com.spring.social_website.auth.jwt.JwtService;
+import com.spring.social_website.auth.token.RefreshTokenService;
 import com.spring.social_website.exception.EmailAlreadyInUseException;
 import com.spring.social_website.user.UserEntity;
 import com.spring.social_website.user.UserRepository;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,15 +25,22 @@ public class AuthService {
     private final JwtService jwtService;
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final RefreshTokenService refreshTokenService;
 
-    public LoginResponseDto login(LoginRequestDto request) {
+    public LoginResponseDto login(LoginRequestDto request, HttpServletResponse response) {
         authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.email(),
                         request.password()));
 
-        String token = jwtService.generateToken(request.email());
-        return new LoginResponseDto(token);
+        UserEntity user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        String refreshToken = refreshTokenService.createAndPersist(user);
+        refreshTokenService.addCookie(refreshToken, response);
+
+        String accessToken = jwtService.generateToken(request.email());
+        return new LoginResponseDto(accessToken);
     }
 
     public RegisterResponseDto register(RegisterRequestDto request) {
