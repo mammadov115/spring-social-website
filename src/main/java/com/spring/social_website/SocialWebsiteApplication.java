bd96fea15960b83fbe69a01cc5cfe7d37989cee6
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-13 22:09:42 +0400

    Format SocialWebsiteApplication.java for consistency and enable Spring Data web support

--- src/main/java/com/spring/social_website/SocialWebsiteApplication.java
+++ src/main/java/com/spring/social_website/SocialWebsiteApplication.java
@@ -3,15 +3,17 @@ package com.spring.social_website;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableJpaAuditing 
-@EnableAsync 
+@EnableJpaAuditing
+@EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class SocialWebsiteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialWebsiteApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialWebsiteApplication.class, args);
+    }
 
 }
