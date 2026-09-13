d10d9e5441153024fdd55fce56f658e23f923701
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-13 12:33:30 +0400

    Add handler for MethodArgumentTypeMismatchException in GlobalExceptionHandler

--- src/main/java/com/spring/social_website/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/social_website/exception/GlobalExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,6 +55,11 @@ public class GlobalExceptionHandler {
         body.put("data", fieldErrors);
         return ResponseEntity.status(HttpStatus.UNPROCESSABLE_CONTENT).body(body);
 
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Map<String, Object>> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(failBody(ex.getName(), "Invalid value: " + ex.getValue()));
     }
 
     @ExceptionHandler(Exception.class)
