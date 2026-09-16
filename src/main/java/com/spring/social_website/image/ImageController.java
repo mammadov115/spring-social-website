72ad7168ef00a3a94e99dfce58bcf199f211b71a
mammadov115 <mehman.mammadov182@gmail.com>
2026-09-16 16:54:25 +0400

    Revert "Enhance ImageController to support sorting by creation date and add OpenApiConfig for API documentation"
    This reverts commit 1fddd989ab1db5c59f40e9ad16053699c3a38b3a.

--- src/main/java/com/spring/social_website/image/ImageController.java
+++ src/main/java/com/spring/social_website/image/ImageController.java
@@ -11,7 +11,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,7 +18,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.data.domain.Sort.Direction;;
 
 import java.io.IOException;
 import java.util.Map;
@@ -70,7 +68,7 @@ public class ImageController {
     @GetMapping
     public ResponseEntity<Map<String, Object>> getFeed(
             @AuthenticationPrincipal String email,
-            @PageableDefault(size = 20, sort = "createdAt", direction = Direction.DESC) Pageable pageable) {
+            @PageableDefault(size = 20) Pageable pageable) {
 
         Page<ImageResponseDto> page = imageService.getFeed(email, pageable);
         return ResponseEntity.ok(Map.of("status", "success", "data", Map.of("images", page)));
@@ -145,7 +143,7 @@ public class ImageController {
     @GetMapping("/bookmarks")
     public ResponseEntity<Map<String, Object>> getBookmarks(
             @AuthenticationPrincipal String email,
-            @PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+            @PageableDefault(size = 20) Pageable pageable) {
 
         Page<ImageResponseDto> page = imageService.getBookmarks(email, pageable);
         return ResponseEntity.ok(Map.of("status", "success", "data", Map.of("images", page)));
